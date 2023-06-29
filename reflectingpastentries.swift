50e45972a07e805376f7778c6b78aeec2b351311
Nat <nataliealcaza11@gmail/com>
2023-06-29 10:10:45 -0700

    calendar

--- reflectingpastentries.swift
+++ reflectingpastentries.swift
@@ -5,16 +5,31 @@
 //  Created by Paige Leung on 6/28/23.
 //
 
+//import SwiftUI
+
 import SwiftUI
 
-struct reflectingpastentries: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct planner: UIViewRepresentable {
+    let interval: DateInterval
+    func makeUIView(context: Context) -> UICalendarView {
+        let view = UICalendarView()
+        view.calendar = Calendar(identifier: .gregorian)
+        view.availableDateRange = interval
+        return view
     }
-}
+    
+    func updateUIView(_ uiView: UICalendarView, context: Context) {
+        
+    }
 
-struct reflectingpastentries_Previews: PreviewProvider {
-    static var previews: some View {
-        reflectingpastentries()
-    }
-}
+//struct reflectingpastentries: View {
+    //var body: some View {
+       // Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  //  }
+//}
+
+//struct reflectingpastentries_Previews: PreviewProvider {
+  //  static var previews: some View {
+    //    reflectingpastentries()
+   // }
+//}
